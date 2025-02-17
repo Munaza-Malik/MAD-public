9afc8dc49bd54d2a15a7f88f073bb4c89d2a67fc
Munaza Malik <44956@students.riphah.edu.pk>
2025-02-17 10:41:54 +0500

    Array of Objects

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('my fist project');
+//console.log('my fist project');
+
+let objArr=[
+    {
+        name: 'Ali' ,
+        age: 21 ,
+        gender: 'Male'
+    },
+    {
+        name: 'Munaza',
+        age: 21 ,
+        gender: 'Female'
+    },
+    {
+        name: 'Amna' ,
+        age: 24 ,
+        gender: 'Female'
+    }
+];
+
+// console.log("First object in the array using the [] notation: ");
+// console.log(objArr[0]);
+
+let names = objArr.map(person => person.name);
+console.log(names);
