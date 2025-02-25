503e18ffbca40436bc6cc93289235cd878e685e9
Munaza Malik <44956@students.riphah.edu.pk>
2025-02-25 21:42:56 +0500

    Array of objects

---
+++ week3.js
@@ -0,0 +1,23 @@
+let objArr=[
+    {
+        name: 'Ali' ,
+        age: 21 ,
+        gender: 'Male'
+    },
+    {
+        name: 'Munaza',
+        age: 21 ,
+        gender: 'Female'
+    },
+    {
+        name: 'Amna' ,
+        age: 24 ,
+        gender: 'Female'
+    }
+];
+
+// console.log("First object in the array using the [] notation: ");
+// console.log(objArr[0]);
+
+let names = objArr.map(person => person.name);
+console.log(names);
