9241aff1a04e38a921ac6e75a7ee6e944cf722f5
Munaza Malik <44956@students.riphah.edu.pk>
2025-02-25 08:52:33 +0500

    Forced update of JS files

---
+++ data.js
@@ -0,0 +1,10 @@
+// data.js
+function getData() {
+    return [
+        { id: 1, name: "Alice", age: 25 },
+        { id: 2, name: "Bob", age: 30 },
+        { id: 3, name: "Charlie", age: 22 }
+    ];
+}
+
+module.exports = getData;
